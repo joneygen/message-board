e7392a00704a25ad1411a4eb0d1e923241dff716
naoya.harata <joneygennumber@gmail.com>
2020-12-02 11:40:07 +0000

    add column

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -48,11 +48,13 @@ class MessagesController extends Controller
     public function store(Request $request)
     {
         $request->validate([
+            'title' => 'required|max:255', 
             'content' => 'required|max:255',
         ]);
         
         // メッセージを作成
         $message = new Message;
+        $message->title = $request->title;
         $message->content = $request->content;
         $message->save();
 
@@ -105,12 +107,14 @@ class MessagesController extends Controller
     {
         // バリデーション
         $request->validate([
+            'title' => 'required|max:255', 
             'content' => 'required|max:255',
         ]);
         
         // idの値でメッセージを検索して取得
         $message = Message::findOrFail($id);
         // メッセージを更新
+        $message->title = $request->title;
         $message->content = $request->content;
         $message->save();
 
